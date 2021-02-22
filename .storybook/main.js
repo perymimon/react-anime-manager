bc83c8bbb71887915377781e5bf9e306140a5032
perym <pery.mimon@gmail.com>
2021-02-22 18:17:20 +0200

    Enable NPM registry support

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  reactOptions: {
+    fastRefresh: true,
+  },
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}

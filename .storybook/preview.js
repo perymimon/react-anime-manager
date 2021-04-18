e79eac43f7cd0baf000b0a392d8e2c3c34cacf34
perym <pery.mimon@gmail.com>
2021-04-19 01:23:31 +0300

    sort story in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,25 @@
+import anysort from 'anysort'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  options: {
+    storySort: (previous, next) => {
+      const [previousStory, previousMeta] = previous
+      const [nextStory, nextMeta] = next
+
+      return anysort(previousMeta.kind, nextMeta.kind, [
+        'Overview/Introduction',
+        'Overview/**',
+        'Usage/**',
+        'Examples/**',
+      ])
+    }
+
+  },
 }

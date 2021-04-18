4e8af45de258df5b1cc300784c8531fafbbd48b5
perym <pery.mimon@gmail.com>
2021-04-18 23:59:08 +0300

    add storybook addons and `scss` support

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,14 +2,23 @@ module.exports = {
     reactOptions: {
         fastRefresh: true,
     },
-    "stories": [
+    stories: [
         "../src/**/*.stories.mdx",
         "../src/**/*.stories.@(js|jsx|ts|tsx)"
     ],
-    "addons": [
+    addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "storybook-addon-jsx",
         '@storybook/addon-storysource'
-    ]
+    ],
+    webpackFinal:async function(config){
+        const path = require('path');
+        config.module.rules.push({
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+            include: path.resolve(__dirname, '../'),
+        });
+        return config;
+    }
 }
